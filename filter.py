30ae669e8191e851679269b5effe00ce79fe1342
Luiza Grigoryan <grigoryan.luiza21@gmail.com>
2020-09-29 02:15:29 +0400

    count added

--- filter.py
+++ filter.py
@@ -1,20 +1,16 @@
 import re
 
 keyword = input('Find: ')
-
+count = int(input ("count: "))
 with open('sample_data.csv') as f:
     data = f.read().split(',')
     data = ''.join(data)
-    
     result = re.findall(keyword + r'[ a-zA-Z]{0,}"', data)
-    
     if result:
         for name in result:
             print(name[0:-1])
+            count -= 1
+            if not count:
+                break
     else:
         print('Not Found')
-        
-    
-    
-    
-    
